12ce1d7fb3943fa657388197559df6e34fb8ea54
sarabs3js <46516570+sarabs3js@users.noreply.github.com>
2022-10-06 16:53:06 +0530

    added video files

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
+var fluent_ffmpeg = require("fluent-ffmpeg");
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => {
+    var mergedVideo = fluent_ffmpeg();
+var videoNames = ['./files/circles.MOV', './files/bemotions.MOV'];
+
+videoNames.forEach(function(videoName){
+    mergedVideo = mergedVideo.addInput(videoName);
+});
+
+mergedVideo.mergeToFile('./mergedVideo.mp4', './tmp/')
+.on('error', function(err) {
+    console.log('Error ' + err.message);
+})
+.on('end', function() {
+    console.log('Finished!');
+    res.render('pages/index');
+});
+    
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
